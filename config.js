ae90c578d4954518cb13af0cf6fa38ef68c5c0ba
Martin Cleaver <mrjcleaver2@gmail.com>
2026-03-12 12:06:57 -0400

    Clear default VIDEO_ID in config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const BRAND_BASE_URL = "/brand";
     ?video=VIDEO_ID
     #VIDEO_ID
 */
-const VIDEO_ID_DEFAULT = "6VP_TDFn5wg";
+const VIDEO_ID_DEFAULT = "";
 
 /*
   Optional meeting link (Zoom / Google Meet etc.)
